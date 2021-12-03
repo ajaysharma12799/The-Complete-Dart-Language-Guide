d8fc17721dc21ec9c3aaf0cf1d04e6bc5e27bace
ajaysharma12799 <ajaysharma12799@gmail.com>
2021-12-03 19:12:54 +0530

    added Loops

---
+++ Loops/loops.dart
@@ -0,0 +1,41 @@
+/* 
+  Loops are used when we need to perform some action repeated manner
+  There are 3 Types of Loops
+  1. for
+  2. while  
+  3. do-while
+*/
+void main() {
+  // 1. for loop
+  /* Find Factorial of Number
+  int factorial = 1;
+  for (int i = 1; i <= 5; i++) {
+    factorial = factorial * i;
+  }
+  print(factorial);
+  */
+  /* Find Table of Number
+  for (var i = 1; i <= 10; i++) {
+    print("2 * $i = ${2 * i}");
+  }
+  */
+
+  // 2. while loop
+  /*
+  var num = 5;
+  var factorial = 1;
+  while (num >= 1) {
+    factorial = factorial * num;
+    num--;
+  }
+  print(factorial);
+  */
+
+  // 3. do-while loop
+  /* var number = 10;
+  do {
+    print(number);
+    number--;
+  } while (number >= 1);
+  */
+}
